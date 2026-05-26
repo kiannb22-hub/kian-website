3803f0ae87cb27bf75ab8d33fa574947a2e59d09
kiannb22-hub <279963659+kiannb22-hub@users.noreply.github.com>
2026-05-26 13:45:40 +0200

    Switch site domain to kbsound.dk

--- src/App.tsx
+++ src/App.tsx
@@ -385,7 +385,7 @@ export default function App() {
           <FadeIn delay={100}>
             <div className="mx-auto max-w-[1000px] overflow-hidden rounded-lg bg-transparent">
               <iframe
-                src={`${CALENDLY_URL}?embed_domain=kbmediax.dk&embed_type=Inline&hide_gdpr_banner=1&primary_color=0069ff`}
+                src={`${CALENDLY_URL}?embed_domain=kbsound.dk&embed_type=Inline&hide_gdpr_banner=1&primary_color=0069ff`}
                 className="block h-[700px] w-full sm:h-[640px]"
                 scrolling="no"
                 frameBorder="0"
